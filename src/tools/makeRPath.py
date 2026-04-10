3dd3d41f56b7b9f400f3d5a3e1dbd7b41ff57a80
Evan Daykin <daykin@frib.msu.edu>
2026-04-10 14:41:15 -0400

    FRIB site modification 9: numbered python in makeRPath

--- src/tools/makeRPath.py
+++ src/tools/makeRPath.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 #*************************************************************************
 # SPDX-License-Identifier: EPICS
 # EPICS BASE is distributed subject to a Software License Agreement found
